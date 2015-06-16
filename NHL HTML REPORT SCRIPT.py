748a4af1b1a86f0c547403f750fa65ee4ecaee0a
Tom Cunningham <foxyronaldo@gmail.com>
2015-06-15 20:14:42 -0400

    Script to pull data from NHL HTML Reports URLS
    This program will download the source code to every HTML report that
    NHL.com has ever published, saving them to a Folder on the desktop.
    Subdirectories are generated for each year.

---
+++ NHL HTML REPORT SCRIPT.py
@@ -0,0 +1,78 @@
+import urllib2
+from urllib2 import HTTPError
+from urllib2 import urlopen
+from cookielib import CookieJar
+from datetime import date
+import re
+import os
+
+
+cj = CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17')]
+
+#Number of games in NHL Season: 1230
+#First year that NHL.com released HTML Reports: 2003-2004
+year = str(20032004)
+gamenumber = str(20001)
+
+def numberofcycles(year):
+	return int(date.today().year) - int(year[:4])
+
+def incyear(year):
+	year = int(year)
+	year = year + 10001
+	return str(year)
+
+def incgamenum(gamenumber):
+	gamenumber = int(gamenumber)
+	if gamenumber<21230:
+		gamenumber += 1
+	else:
+		gamenumber = 20001
+
+	return str(gamenumber)
+
+
+def nhlhtmlsheeturl(year, gamenumber):
+	url = "http://www.nhl.com/scores/htmlreports/" + year + "/PL0" + gamenumber + ".HTM"
+	return url
+
+def createfile(yeardirectory):
+	
+	if not os.path.exists(yeardirectory):
+    		os.makedirs(yeardirectory)
+
+def yearwithhyphen(year):
+	year = year[:4] + "-" + year[-4:]
+	return year
+
+def sourcecodetotext(url, gamenumber, year):
+
+	try:
+		webPage=urllib2.urlopen(url)
+		rawSrc = makestringpath(yearwithhyphen(year)) + "\\" + gamenumber + ".txt"
+		wRawSrc = open(rawSrc, "w")
+		wPageSrc=webPage.read()
+		webPage.close()
+		wRawSrc.write(wPageSrc)
+
+	except HTTPError:
+		print "Game %s in year %s does not exist in HTML Report database" %(gamenumber, year)
+
+def makestringpath(year):
+	directory = 'C:\\Users\\Owner\\Desktop\\NHL HTML Reports\\' + year
+	return directory
+
+
+for _ in range(1230*numberofcycles(year)):
+
+	url = nhlhtmlsheeturl(year, gamenumber)
+	sourcecodetotext(url, gamenumber, year)
+	print gamenumber
+	gamenumber = incgamenum(gamenumber)
+
+	if int(gamenumber) == 20001:
+		year = incyear(year)
+		createfile(makestringpath(yearwithhyphen(year)))
+		print yearwithhyphen(year)
